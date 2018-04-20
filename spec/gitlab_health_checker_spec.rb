29eafcb3708a3680c7b010d0ad8bf8eb52cd1953
whyarkadas <38353086+whyarkadas@users.noreply.github.com>
2018-04-20 11:27:02 +0300

    Update gitlab_health_checker_spec.rb

--- spec/gitlab_health_checker_spec.rb
+++ spec/gitlab_health_checker_spec.rb
@@ -2,8 +2,7 @@ RSpec.describe GitlabHealthChecker do
   it "has a version number" do
     expect(GitlabHealthChecker::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "make choices" do
+    expect (GitlabHealthChecker::GitlabHealthCheck.ping('3')).to eql("You have to choice 1 or 2")
   end
 end
